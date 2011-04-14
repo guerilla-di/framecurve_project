8e0a18fa04ba10b6c15a4d192118ed5ec2b3750c
Julik <me@julik.nl>
2011-04-14 11:21:08 +0200

    Apply to all selected nodes

--- lib/scripts/nuke/applyFramecurve.py
+++ lib/scripts/nuke/applyFramecurve.py
@@ -1,4 +1,4 @@
-from __future__ import with_statement # Only needed in Python 2.5
+from __future__ import with_statement
 import nuke, nukescripts, os, re
 
 # On the current node, add a userknob for the framecurve
@@ -9,19 +9,19 @@ def add_framecurve(onNode):
     onNode["framecurve"].setAnimated()
     onNode["framecurve"].setValueAt(1.0, 1)
     onNode["label"].setValue("TW to [value framecurve]")
+
+# We want our retime knob to appear in a separate tab
+def organize_retime_tab(onNode):
+    pass
     
 def apply_framecurve(inNode, toKnobName):
-    k = inNode.knob(toKnobName)
-    if not k.visible() or not k.isAnimated():
-        return
-    # Apply the magic framecurve expr!
-    k.setExpression("curve(framecurve(frame))")
+    pass
     
-def get_first_selected_node():
-    selected = filter(lambda n: n.Class() != "Viewer" and n.name() != "VIEWER_INPUT", nuke.selectedNodes())
-    if len(selected) == 0:
-        raise Exception, "No nodes were selected"
-    return selected[0]
+# TODO - does not work yet!
+def make_keyframes_linear(curve):
+    for key in curve.keys():
+        print key.interpolation
+        key.interpolation = nuke.LINEAR
 
 def animate_framecurve_from_file(fc_path, to_node):
     with open(fc_path) as fc_file:
@@ -29,32 +29,36 @@ def animate_framecurve_from_file(fc_path, to_node):
             if line.startswith("#"):
                 pass
             else:
+                # Parse out the two values
                 at_and_v = line.split("\t")
                 at_frame, use_frame = int(at_and_v[0]), float(at_and_v[1])
                 # Set the keyframe on the framecurve
                 to_node["framecurve"].setValueAt(use_frame, at_frame) #, index=1, view=1)
     
     # Change the framecurve KFs to give linear interp - TODO this doesnt work yet
-    curve = to_node["framecurve"].animation(0)
-    for key in curve.keys():
-        print key.interpolation
-        key.interpolation = nuke.LINEAR
-        
-# keyframe it from the framecurve file
-me = get_first_selected_node()
-knob_names = me.knobs()
-if not "framecurve" in knob_names:
-    add_framecurve(me)
+    make_keyframes_linear(to_node["framecurve"].animation(0))
+
+def apply_framecurve(toNode, framecurve_path):
+    if not "framecurve" in toNode.knobs():
+        add_framecurve(toNode)
+        organize_retime_tab(toNode)
     
-# Couple the animations to the framecurve
-for knob_name in me.knobs():
-    if knob_name == "framecurve":
-        pass
-    else:
-        apply_framecurve(me, knob_name)
+    # Apply the file at path
+    animate_framecurve_from_file(framecurve_path, toNode)
+    
+    # Couple the animations to the framecurve
+    for knob_name in toNode.knobs():
+        if knob_name == "framecurve":
+            pass
+        else:
+            k = toNode.knob(knob_name)
+            if k.visible() and k.isAnimated():
+                # Apply the magic framecurve expr!
+                k.setExpression("curve(framecurve(frame))")
+
 
 # Load the animation
-# framecurve_path = nuke.getFilename("Select the framecurve file", "*.framecurve.txt", default="/", favorites="", type="", multiple=False)
-framecurve_path = "/test_export.framecurve.txt"
-
-animate_framecurve_from_file(framecurve_path, me)
+framecurve_path = nuke.getFilename("Select the framecurve file", "*.framecurve.txt", default="/", favorites="", type="", multiple=False)
+selected = filter(lambda n: n.Class() != "Viewer" and n.name() != "VIEWER_INPUT", nuke.selectedNodes())
+for n in selected:
+    apply_framecurve(n, framecurve_path) 
